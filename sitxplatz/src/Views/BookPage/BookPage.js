37553bafadbcc2026b21e4fe8e710ff5c473cfb1
DuskoPeric <Dusko.Peric@homegate.ch>
2023-03-30 16:55:22 +0200

    Trust me, it's not badly written. It's just way above your head.

--- sitxplatz/src/Views/BookPage/BookPage.js
+++ sitxplatz/src/Views/BookPage/BookPage.js
@@ -1,8 +1,30 @@
 
 
 import "./BookPage.scss";
+import React, { useState } from 'react';
 
 const BookPage = () => {
+
+    const [data, setData] = useState({
+        "o1-1":false,
+        "o1-2":false,
+        "o1-3":false,
+        "o1-4":false,
+        "o1-5":false,
+        "o1-6":false,
+        "o1-7":false,
+        "o1-8":false,
+        "o1-9":false,
+        "o1-10":false,
+        "o1-11":false,
+        "o1-12":false,
+        "o1-13":false,
+        "o1-14":false,
+        "o1-15":false,
+        "o1-16":false,
+        "o1-17":false,
+        "o1-18":false,
+    })
 
 
     return (
@@ -12,26 +34,26 @@ const BookPage = () => {
           <div className="row">
             <div className="office vertical big5">
               <div className="chair-row long5">
-                  <div className="chair revert reserved"></div>
-                  <div className="chair revert"></div>
-                  <div className="chair revert"></div>
-                  <div className="chair revert"></div>
-                  <div className="chair revert"></div>
-                <div className="chair"></div>
-                <div className="chair"></div>
-                <div className="chair"></div>
-                <div className="chair"></div>
-                <div className="chair"></div>
+                  <div id="o1-1" className={`chair revert ${data['o1-1']?'reserved':null}`}></div>
+                  <div id="o1-2"  className={`chair revert ${data['o1-2']?'reserved':null}`}></div>
+                  <div id="o1-3"  className={`chair revert ${data['o1-3']?'reserved':null}`}></div>
+                  <div  id="o1-4" className={`chair revert ${data['o1-4']?'reserved':null}`}></div>
+                  <div  id="o1-5" className={`chair revert ${data['o1-5']?'reserved':null}`}></div>
+                <div  id="o1-6" className={`chair  ${data['o1-6']?'reserved':null}`}></div>
+                <div id="o1-7"  className={`chair  ${data['o1-7']?'reserved':null}`}></div>
+                <div id="o1-8"  className={`chair  ${data['o1-8']?'reserved':null}`}></div>
+                <div id="o1-9"  className={`chair  ${data['o1-9']?'reserved':null}`}></div>
+                <div id="o1-10"  className={`chair  ${data['o1-10']?'reserved':null}`}></div>
               </div>
               <div className="chair-row long4">
-                <div className="chair revert"></div>
-                <div className="chair revert"></div>
-                <div className="chair revert"></div>
-                <div className="chair revert"></div>
-                <div className="chair"></div>
-                <div className="chair"></div>
-                <div className="chair"></div>
-                <div className="chair"></div>
+                <div id="o1-11" className={`chair revert ${data['o1-11']?'reserved':null}`}></div>
+                <div id="o1-12" className={`chair revert ${data['o1-12']?'reserved':null}`}></div>
+                <div id="o1-13" className={`chair revert ${data['o1-13']?'reserved':null}`}></div>
+                <div id="o1-14" className={`chair revert ${data['o1-14']?'reserved':null}`}></div>
+                <div id="o1-15" className={`chair  ${data['o1-15']?'reserved':null}`}></div>
+                <div id="o1-16" className={`chair  ${data['o1-16']?'reserved':null}`}></div>
+                <div id="o1-17" className={`chair  ${data['o1-17']?'reserved':null}`}></div>
+                <div id="o1-18" className={`chair  ${data['o1-18']?'reserved':null}`}></div>
               </div>
             </div>
             <div className="office horizontal wall-left big3">
